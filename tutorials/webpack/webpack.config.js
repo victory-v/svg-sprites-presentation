241827e3ec5599e23d139a1a25eee4cfac38d171
vivasile <Viktoriia.Vasileva@t-systems.com>
2019-03-14 18:20:14 +0300

    Tutorial for webpack

---
+++ tutorials/webpack/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const SpritePlugin = require('svg-sprite-loader/plugin');
+
+
+module.exports = {
+  context: __dirname,
+
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'build')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-sprite-loader'
+          },
+          {
+            loader: 'svgo-loader',
+            options: {
+              plugins: [
+                {removeTitle: true},
+                {convertColors: {shorthex: false}},
+                {removeUselessStrokeAndFill:true},
+                {removeStyleElement:true},
+                {removeAttrs:{ attrs: ['fill', 'stroke'] } },
+              ]
+            }
+          }
+        ],
+      }
+    ]
+  },
+
+  plugins: [
+    new SpritePlugin()
+  ]
+};
